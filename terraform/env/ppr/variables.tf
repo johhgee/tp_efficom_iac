15ac4c734f79672e5a7968f3b1324634ce3651f4
Jonathan O.G <55450383+johhgee@users.noreply.github.com>
2024-05-17 17:54:48 +0200

    First commit

---
+++ terraform/env/ppr/variables.tf
@@ -0,0 +1,19 @@
+variable "default_region" {
+  type = string
+  default = "europe-west-1"
+}
+
+variable "project" {
+  type = string
+  default = "Projet_TP_IAC"
+}
+
+variable "billing_account" {
+  type = string
+  default = "xxxxxxxxxxxxxxxx"
+}
+
+variable "default_zone" {
+  type = string
+  default = "europe-west-1b"
+}
